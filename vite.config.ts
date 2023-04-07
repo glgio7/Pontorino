0af8b4b80366070cfebee5ba35cf9e7da9de15e8
GLGIO7 <giovanelucas00@outlook.com>
2023-04-07 18:28:36 -0300

    fix: process is not defined

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import replace from "@rollup/plugin-replace";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		react(),
-		replace({
-			"process.env.VITE_APP_API_KEY": JSON.stringify(
-				process.env.VITE_APP_API_KEY
-			),
-		}),
-	],
+	plugins: [react()],
+	build: {
+		sourcemap: true,
+	},
+	envDir: "./",
+	define: {
+		"process.env": {},
+	},
 });
