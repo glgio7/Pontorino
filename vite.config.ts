8607c2da226248731216868f26758ced3b7facf3
GLGIO7 <giovanelucas00@outlook.com>
2023-04-07 18:15:04 -0300

    fix: process is not defined

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import * as dotenv from "dotenv";
 
-// https://vitejs.dev/config/
+dotenv.config();
+
 export default defineConfig({
 	plugins: [react()],
+	build: {
+		rollupOptions: {
+			output: {
+				inlineDynamicImports: true,
+				manualChunks: {
+					dotenv: ["dotenv"],
+				},
+			},
+		},
+	},
 });
